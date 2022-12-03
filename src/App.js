cdeb5de9c29f8816da135f11c04cec34fffcd07f
bunnygadu <MY_NAME@example.com>
2022-12-03 18:45:26 +0530

    namechange

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import {Route, Routes} from 'react-router-dom'
-import Deepak from './components/Deepak'
-import Header from './components/Header'
-import SaiNaveen from './components/SaiNaveen'
-import Sathwik from './components/Sathwik'
-import Venkat from './components/Venkat'
-import Ramya from './components/Ramya'
-import Dhathrija from './components/Dhathrija'
-
-const App =() => (
-  <>
-  <Header />
-   <Routes>
-  <Route exact path="/" element={<Deepak />} />
-  <Route exact path="/sainaveen" element={<SaiNaveen />} />
-  <Route exact path="/sathwik" element={<Sathwik />} />
-  <Route exact path="/venkat" element={<Venkat />} />
-  <Route exact path="/ramya" element={<Ramya />} /> 
-  <Route exact path="/dhathrija" element={<Dhathrija />} /> 
-  </Routes> 
-  </>
-)
-export default App
+ import React, { Component } from 'react'
+ 
+ export default class App extends Component {
+   render() {
+     return (
+       <div>
+         <h1 id ="v">Venkat</h1>
+       </div>
+     )
+   }
+ }
+ 
